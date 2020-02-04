c0ed8e57cc3faa532718811a23e7e0857324b0e8
Chloe Foronda <caforonda103@yahoo.com>
2020-02-04 00:06:51 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,4 @@
 class User < Student
   
-  attr_accessor :first_name, :last_name
-  
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
-  
   
 end
